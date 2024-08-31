8f649456a4ecf2ebfbce39c457be25ad58f71f1c
gdanko <Gary_Danko@intuit.com>
2024-08-30 17:04:03 -0700

    Add a plugin to show the number of available system updates

---
+++ System/gdanko-system-SystemUpdates.15m.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# <xbar.title>System Updates</xbar.title>
+# <xbar.version>v0.1.0</xbar.version>
+# <xbar.author>Gary Danko</xbar.author>
+# <xbar.author.github>gdanko</xbar.author.github>
+# <xbar.desc>This plugin displays the number of available system updates.</xbar.desc>
+# <xbar.dependencies>python</xbar.dependencies>
+# <xbar.abouturl>https://github.com/gdanko/xbar-plugins/blob/main/System/gdanko-system-SystemUpdates.15m.py</xbar.abouturl>
+
+import subprocess
+
+def main():
+    p = subprocess.Popen(
+        ['softwareupdate', '--list'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    stdout, stderr = p.communicate()
+    if p.returncode == 0:
+        updates = 0
+        lines = stdout.split('\n')
+        for line in lines:
+            if '* Label' in line:
+                updates += 1
+    
+    print(f'Updates: {updates}')
+    
+if __name__ == '__main__':
+    main()
