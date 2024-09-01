b768d4c25911ac5c04b3c1e4dd4bb18db1e84156
gdanko <Gary_Danko@intuit.com>
2024-09-01 13:05:49 -0700

    Add CPU type to the output

--- System/gdanko-system-CpuPercent.2s.py
+++ System/gdanko-system-CpuPercent.2s.py
@@ -10,19 +10,33 @@
 
 from collections import namedtuple
 from math import ceil
+import subprocess
 import time
 from pprint import pprint
 
 def pad_float(number):
     return '{:.2f}'.format(float(number))
 
-def get_percent_stats_tuple(cpu='cpu-total', user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
-    cpu_percent = namedtuple('cpu_percent', 'cpu user system idle nice iowait irq softirq steal guest guestnice')
-    return cpu_percent(cpu=cpu, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
+def get_percent_stats_tuple(cpu='cpu-total', cpu_type=None, user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
+    cpu_percent = namedtuple('cpu_percent', 'cpu cpu_type user system idle nice iowait irq softirq steal guest guestnice')
+    return cpu_percent(cpu=cpu, cpu_type=cpu_type, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
 
-def get_time_stats_tuple(cpu='cpu-total', user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
-    cpu_times = namedtuple('cpu_times', 'cpu user system idle nice iowait irq softirq steal guest guestnice')
-    return cpu_times(cpu=cpu, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
+def get_time_stats_tuple(cpu='cpu-total', cpu_type=None, user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
+    cpu_times = namedtuple('cpu_times', 'cpu cpu_type user system idle nice iowait irq softirq steal guest guestnice')
+    return cpu_times(cpu=cpu, cpu_type=cpu_type, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
+
+def get_cpu_type():
+    p = subprocess.Popen(
+        ['/usr/sbin/sysctl', '-n', 'machdep.cpu.brand_string'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    stdout, stderr = p.communicate()
+    if p.returncode == 0:
+        return stdout.strip()
+    else:
+        return None
 
 def cpu_time_deltas(t1, t2):
     return get_time_stats_tuple(
@@ -48,6 +62,7 @@ def calculate(t1, t2):
 
     return get_percent_stats_tuple(
         cpu         = t1.cpu,
+        cpu_type    = t1.cpu_type,
         user        = ceil(min(max(0.0, (times_delta.user * scale)), 100.0) * 100) / 100,
         system      = ceil(min(max(0.0, (times_delta.system * scale)), 100.0) * 100) / 100,
         idle        = ceil(min(max(0.0, (times_delta.idle * scale)), 100.0) * 100) / 100,
@@ -76,6 +91,8 @@ def combine_stats(cpu_time_stats):
 def gather_cpu_info():
     try:
         from psutil import cpu_times
+
+        cpu_type = get_cpu_type()
 
         interval = 1.0
         blocking = False
@@ -106,34 +123,35 @@ def gather_cpu_info():
         combined_t2 = combine_stats(last_per_cpu_times2)
 
         # Get the stats for the combined
-        list_t1_combined.append(get_time_stats_tuple(user=combined_t1.user, system=combined_t1.system, nice=combined_t1.nice, idle=combined_t1.idle))
-        list_t2_combined.append(get_time_stats_tuple(user=combined_t2.user, system=combined_t2.system, nice=combined_t2.nice, idle=combined_t2.idle))
+        list_t1_combined.append(get_time_stats_tuple(cpu_type=cpu_type, user=combined_t1.user, system=combined_t1.system, nice=combined_t1.nice, idle=combined_t1.idle))
+        list_t2_combined.append(get_time_stats_tuple(cpu_type=cpu_type, user=combined_t2.user, system=combined_t2.system, nice=combined_t2.nice, idle=combined_t2.idle))
         output_combined.append(calculate(list_t1_combined[0], list_t2_combined[0]))
 
         # Get the stats for the individual
         for i, cpu_instance in enumerate(t1):
-            list_t1_individual.append(get_time_stats_tuple(cpu=i, user=cpu_instance.user, system=cpu_instance.system, nice=cpu_instance.nice, idle=cpu_instance.idle))
+            list_t1_individual.append(get_time_stats_tuple(cpu=i, cpu_type=cpu_type, user=cpu_instance.user, system=cpu_instance.system, nice=cpu_instance.nice, idle=cpu_instance.idle))
         for i, cpu_instance in enumerate(last_per_cpu_times2):
-            list_t2_individual.append(get_time_stats_tuple(cpu=i, user=cpu_instance.user, system=cpu_instance.system, nice=cpu_instance.nice, idle=cpu_instance.idle))
+            list_t2_individual.append(get_time_stats_tuple(cpu=i, cpu_type=cpu_type, user=cpu_instance.user, system=cpu_instance.system, nice=cpu_instance.nice, idle=cpu_instance.idle))
         for i in range(len(t1)):
             output_individual.append(calculate(list_t1_individual[i], list_t2_individual[i]))
 
-        return output_combined, output_individual
+        return output_combined, output_individual, cpu_type
 
     except ModuleNotFoundError:
         print('Error: missing "psutil" library.')
         print('---')
         import sys
         import subprocess
-        subprocess.run('pbcopy', universal_newlines=True,
-                       input=f'{sys.executable} -m pip install psutil')
+        subprocess.run('pbcopy', universal_newlines=True, input=f'{sys.executable} -m pip install psutil')
         print('Fix copied to clipboard. Paste on terminal and run.')
 
 def main():
-    output_combined, output_individual = gather_cpu_info()
+    output_combined, output_individual, cpu_type = gather_cpu_info()
 
     print(f'CPU: user {pad_float(output_combined[0].user)}%, sys {pad_float(output_combined[0].system)}%, idle {pad_float(output_combined[0].idle)}%')
     print('---')
+    if cpu_type is not None:
+        print(f'Processor: {cpu_type}')
     for cpu in output_individual:
         print(f'CPU {cpu.cpu}: user {cpu.user}%, sys {cpu.system}%, idle {cpu.idle}%')
 
