ff902d90a638595f7d9a307826833e7b179ff20c
gdanko <Gary_Danko@intuit.com>
2024-09-06 11:15:11 -0700

    Remove unneeded functions

--- System/gdanko-system-CpuPercent.2s.py
+++ System/gdanko-system-CpuPercent.2s.py
@@ -31,10 +31,6 @@ def pad_float(number):
 def get_timestamp(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %k:%M:%S')
 
-def get_percent_stats_tuple(cpu='cpu-total', cpu_type=None, user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
-    cpu_percent = namedtuple('cpu_percent', 'cpu cpu_type user system idle nice iowait irq softirq steal guest guestnice')
-    return cpu_percent(cpu=cpu, cpu_type=cpu_type, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
-
 def get_time_stats_tuple(cpu='cpu-total', cpu_type=None, user=0.0, system=0.0, idle=0.0, nice=0.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guestnice=0.0):
     cpu_times = namedtuple('cpu_times', 'cpu cpu_type user system idle nice iowait irq softirq steal guest guestnice')
     return cpu_times(cpu=cpu, cpu_type=cpu_type, user=user, system=system, idle=idle, nice=nice, iowait=iowait, irq=irq, softirq=softirq, steal=steal, guest=guest, guestnice=guestnice)
@@ -46,48 +42,11 @@ def get_cpu_type():
         stderr=subprocess.PIPE,
         text=True,
     )
-    stdout, stderr = p.communicate()
+    stdout, _ = p.communicate()
     if p.returncode == 0:
         return stdout.strip()
     else:
         return None
-
-def cpu_time_deltas(t1, t2):
-    return get_time_stats_tuple(
-        user        = round(max(0, (t2.user - t1.user)), 2),
-        system      = round(max(0, (t2.system - t1.system)), 2),
-        idle        = round(max(0, (t2.idle - t1.idle)), 2),
-        nice        = round(max(0, (t2.nice - t1.nice)), 2),
-        iowait      = round(max(0, (t2.iowait - t1.iowait)), 2),
-        irq         = round(max(0, (t2.irq - t1.irq)), 2),
-        softirq     = round(max(0, (t2.softirq - t1.softirq)), 2),
-        steal       = round(max(0, (t2.steal - t1.steal)), 2),
-        guest       = round(max(0, (t2.guest - t1.guest)), 2),
-        guestnice   = round(max(0, (t2.guestnice - t1.guestnice)), 2),
-    )
-
-def cpu_total_time(times_delta):
-    return times_delta.user + times_delta.nice + times_delta.system + times_delta.idle
-
-def calculate(t1, t2):
-    times_delta = cpu_time_deltas(t1, t2)
-    all_delta = cpu_total_time(times_delta)
-    scale = 100.0 / max(1, all_delta)
-
-    return get_percent_stats_tuple(
-        cpu         = t1.cpu,
-        cpu_type    = t1.cpu_type,
-        user        = ceil(min(max(0.0, (times_delta.user * scale)), 100.0) * 100) / 100,
-        system      = ceil(min(max(0.0, (times_delta.system * scale)), 100.0) * 100) / 100,
-        idle        = ceil(min(max(0.0, (times_delta.idle * scale)), 100.0) * 100) / 100,
-        nice        = ceil(min(max(0.0, (times_delta.nice * scale)), 100.0) * 100) / 100,
-        iowait      = ceil(min(max(0.0, (times_delta.iowait * scale)), 100.0) * 100) / 100,
-        irq         = ceil(min(max(0.0, (times_delta.irq * scale)), 100.0) * 100) / 100,
-        softirq     = ceil(min(max(0.0, (times_delta.softirq * scale)), 100.0) * 100) / 100,
-        steal       = ceil(min(max(0.0, (times_delta.steal * scale)), 100.0) * 100) / 100,
-        guest       = ceil(min(max(0.0, (times_delta.guest * scale)), 100.0) * 100) / 100,
-        guestnice   = ceil(min(max(0.0, (times_delta.guestnice * scale)), 100.0) * 100) / 100,
-    )
 
 def combine_stats(cpu_time_stats, cpu_type):
     idle      = 0.0
@@ -113,9 +72,10 @@ def gather_cpu_info():
     max_cpu_freq = cpu_freq().max if cpu_freq().max is not None else None
     individual_cpu_pct = []
     combined_cpu_pct = []
-    interval = 1.0
 
-    individual_cpu_percent = cpu_times_percent(interval=interval, percpu=True)
+    individual_cpu_percent = cpu_times_percent(interval=1.0, percpu=True)
+
+    # Create more detailed tuples for the cpu_times_percent() output
     for i, cpu_instance in enumerate(individual_cpu_percent):
         individual_cpu_pct.append(get_time_stats_tuple(cpu=i, cpu_type=cpu_type, user=cpu_instance.user, system=cpu_instance.system, nice=cpu_instance.nice, idle=cpu_instance.idle))
     combined_cpu_pct.append(combine_stats(individual_cpu_pct, cpu_type))
