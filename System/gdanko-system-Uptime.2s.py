6882a0e3690784ca1e60a936c1fbf26501ee7bb5
gdanko <Gary_Danko@intuit.com>
2024-09-15 14:55:46 -0700

    Remove years from the uptime calculation because of leap year

--- System/gdanko-system-Uptime.2s.py
+++ System/gdanko-system-Uptime.2s.py
@@ -27,29 +27,24 @@ except ModuleNotFoundError:
 def get_timestamp(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %k:%M:%S')
 
-def get_duration_tuple(years=0, days=0, hours=0, minutes=0, seconds=0):
-    duration = namedtuple('duration', 'years days hours minutes seconds')
-    return duration(years=years, days=days, hours=hours, minutes=minutes, seconds=seconds)
+def get_duration_tuple(days=0, hours=0, minutes=0, seconds=0):
+    duration = namedtuple('duration', 'days hours minutes seconds')
+    return duration(days=days, hours=hours, minutes=minutes, seconds=seconds)
 
 def get_plural(count, string):
     return string if count == 1 else f'{string}s'
 
 def get_duration(boot_time):
-    years, days, hours, minutes, seconds = 0, 0, 0, 0, 0
+    days, hours, minutes, seconds = 0, 0, 0, 0
     delta = datetime.datetime.fromtimestamp(int(time.time())) - datetime.datetime.fromtimestamp(boot_time)
-    if delta.days > 365:
-        years, days = divmod(delta.days, 365)
-    else:
-        days = delta.days
+    days = delta.days
     hours, remainder = divmod(delta.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
-    return get_duration_tuple(years=years, days=days, hours=hours, minutes=minutes, seconds=seconds)
+    return get_duration_tuple(days=days, hours=hours, minutes=minutes, seconds=seconds)
 
 def main():
     uptime = []
     duration = get_duration(boot_time())
-    if duration.years > 0:
-        uptime.append(f'{duration.years} {get_plural(duration.years, "year")}')
     if duration.days > 0:
         uptime.append(f'{duration.days} {get_plural(duration.days, "day")}')
     uptime.append(f'{str(duration.hours).zfill(2)}:{str(duration.minutes).zfill(2)}')
