e2d91718addc17653368c295a5510d10b5f05da8
gdanko <Gary_Danko@intuit.com>
2025-01-07 14:51:39 -0800

    Remove the Unit env var and use format_number() instead

--- System/gdanko-system-MemoryUsage.2s.py
+++ System/gdanko-system-MemoryUsage.2s.py
@@ -7,7 +7,6 @@
 # <xbar.desc>Show system memery usage in the format used/total</xbar.desc>
 # <xbar.dependencies>python</xbar.dependencies>
 # <xbar.abouturl>https://github.com/gdanko/xbar-plugins/blob/main/System/gdanko-system-MemoryUsage.2s.py</xbar.abouturl>
-# <xbar.var>string(VAR_MEM_USAGE_UNIT="Gi"): The unit to use. [K, Ki, M, Mi, G, Gi, T, Ti, P, Pi, E, Ei]</xbar.var>
 # <xbar.var>string(VAR_MEM_USAGE_CLICK_TO_KILL="false"): Will clicking a member of the top offender list attempt to kill it?</xbar.var>
 # <xbar.var>string(VAR_MEM_USAGE_KILL_SIGNAL=<int>): The Darwin kill signal to use when killing a process</xbar.var>
 # <xbar.var>string(VAR_MEM_USAGE_MAX_CONSUMERS=<int>): Maximum number of offenders to display</xbar.var>
@@ -18,7 +17,6 @@ import getpass
 import json
 import os
 import re
-import shutil
 import signal
 import subprocess
 import sys
@@ -119,19 +117,15 @@ def update_setting(plugin, key, value):
                 write_config(jsonfile, contents)
 
 def get_defaults():
-    valid_units = ['K', 'Ki', 'M', 'Mi', 'G', 'Gi', 'T', 'Ti', 'P', 'Pi', 'E', 'Ei']
-    unit = os.getenv('VAR_MEM_USAGE_UNIT', 'Gi') 
-    if not unit in valid_units:
-        unit = 'Gi'
     click_to_kill = read_config('VAR_MEM_USAGE_CLICK_TO_KILL', 'false')
     click_to_kill = True if click_to_kill == 'true' else False
     signal = read_config('VAR_MEM_USAGE_KILL_SIGNAL', 'SIGQUIT')
     max_consumers = read_config('VAR_MEM_USAGE_MAX_CONSUMERS', 30)
 
-    return unit, click_to_kill, signal, max_consumers
+    return click_to_kill, signal, max_consumers
 
 def get_memory_details():
-    command = f'{shutil.which("system_profiler")} SPMemoryDataType -json'
+    command = f'system_profiler SPMemoryDataType -json'
     output = get_command_output(command)
     if output:
         try:
@@ -153,25 +147,43 @@ def get_command_output(command):
 
 def get_top_memory_usage():
     memory_info = []
-    command = f'/bin/ps -axm -o rss,pid,user,comm | {shutil.which("tail")} -n+2 | {shutil.which("sort")} -rn -k 1'
+    command = f'/bin/ps -axm -o rss,pid,user,comm | tail -n+2'
     output = get_command_output(command)
     if output:
         lines = output.strip().split('\n')
         for line in lines:
             match = re.search(r'^(\d+)\s+(\d+)\s+([A-Za-z0-9\-\.\_]+)\s+(.*)$', line)
             if match:
-                memory_usage = int(match.group(1)) * 1024
+                bytes = int(match.group(1)) * 1024
                 pid = match.group(2)
                 user = match.group(3)
                 command_name = match.group(4)
-                if memory_usage > 0:
-                    memory_info.append({'command': command_name, 'memory_usage': memory_usage, 'pid': pid, 'user': user})
-        return memory_info
+                if bytes > 0:
+                    memory_info.append({'command': command_name, 'bytes': bytes, 'pid': pid, 'user': user})
+
+    return sorted(memory_info, key=lambda item: item['bytes'], reverse=True)
 
 def get_disabled_flag(process_owner, click_to_kill):
     return ('false' if process_owner == getpass.getuser() else 'true') if click_to_kill else 'true'
 
+def format_number(size):
+    factor = 1024
+    bytes = factor
+    megabytes = bytes * factor
+    gigabytes = megabytes * factor
+    if size < gigabytes:
+        if size < megabytes:
+            if size < bytes:
+                return f'{size} B'
+            else:
+                return byte_converter(size, "Ki")
+        else:
+            return byte_converter(size, "Mi")
+    else:
+        return byte_converter(size, "Gi")
+
 def main():
+    unit = 'Gi'
     os.environ['PATH'] = '/bin:/sbin:/usr/bin:/usr/sbin'
     plugin = os.path.abspath(sys.argv[0])
     args = configure()
@@ -184,28 +196,28 @@ def main():
     elif args.max_consumers > 0:
         update_setting(plugin, 'VAR_MEM_USAGE_MAX_CONSUMERS', args.max_consumers)
     
-    unit, click_to_kill, signal, max_consumers = get_defaults()
+    click_to_kill, signal, max_consumers = get_defaults()
     command_length = 125
     font_name = 'Andale Mono'
     font_size = 13
     font_data = f'size="{font_size}" font="{font_name}"'
     memory_type, memory_brand, err = get_memory_details()
     mem = virtual_memory()
-    used = byte_converter(mem.used, unit)
-    total = byte_converter(mem.total, unit)
+    used = format_number(mem.used)
+    total = format_number(mem.total)
     print(f'Memory: {used} / {total}')
     print('---')
     print(f'Updated {get_timestamp(int(time.time()))}')
     print('---')
     if not err:
         print(f'Memory: {memory_brand} {memory_type}')
-    print(f'Total: {byte_converter(mem.total, unit)}')
-    print(f'Available: {byte_converter(mem.available, unit)}')
-    print(f'Used: {byte_converter(mem.used, unit)}')
-    print(f'Free: {byte_converter(mem.free, unit)}')
-    print(f'Active: {byte_converter(mem.active, unit)}')
-    print(f'Inactive: {byte_converter(mem.inactive, unit)}')
-    print(f'Wired: {byte_converter(mem.wired, unit)}')
+    print(f'Total: {format_number(mem.total)}')
+    print(f'Available: {format_number(mem.available)}')
+    print(f'Used: {format_number(mem.used)}')
+    print(f'Free: {format_number(mem.free)}')
+    print(f'Active: {format_number(mem.active)}')
+    print(f'Inactive: {format_number(mem.inactive)}')
+    print(f'Wired: {format_number(mem.wired)}')
 
     top_memory_consumers = get_top_memory_usage()
     if len(top_memory_consumers) > 0:
@@ -215,12 +227,14 @@ def main():
         consumer_total = 0
         for consumer in top_memory_consumers:
             command = consumer['command']
-            memory_usage = consumer['memory_usage']
+            bytes = consumer['bytes']
             pid = consumer['pid']
             user = consumer['user']
-            consumer_total += memory_usage
-            print(f'--{":skull: " if click_to_kill else ""}{byte_converter(memory_usage, "G").rjust(8)} - {command} | length={command_length} | {font_data} | trim=false | shell=/bin/sh | param1="-c" | param2="kill -{get_signal_map()[signal]} {pid}" | disabled={get_disabled_flag(user, click_to_kill)}')
-        print(f'--Total: {byte_converter(consumer_total, "G")} | {font_data}')
+            consumer_total += bytes
+            # Auto-set the width based on the widest member
+            padding_width = 11
+            print(f'--{":skull: " if click_to_kill else ""}{format_number(bytes).rjust(padding_width)} - {command} | length={command_length} | {font_data} | trim=false | shell=/bin/sh | param1="-c" | param2="kill -{get_signal_map()[signal]} {pid}" | disabled={get_disabled_flag(user, click_to_kill)}')
+        print(f'--Total: {format_number(consumer_total)} | {font_data}')
     print('---')
     print('Settings')
     print(f'{"--Disable" if click_to_kill else "--Enable"} "Click to Kill" | shell="{plugin}" | param1={"--disable" if click_to_kill else "--enable"} | terminal=false | refresh=true')
