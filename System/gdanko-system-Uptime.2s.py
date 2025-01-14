1ea69c1c922829bf5560f90863f83efb8aeae06a
gdanko <Gary_Danko@intuit.com>
2025-01-13 18:35:11 -0800

    Use sysctl instead of psutil

--- System/gdanko-system-Uptime.2s.py
+++ System/gdanko-system-Uptime.2s.py
@@ -8,49 +8,68 @@
 # <xbar.dependencies>python</xbar.dependencies>
 # <xbar.abouturl>https://github.com/gdanko/xbar-plugins/blob/main/System/gdanko-system-Uptime.2s.py</xbar.abouturl>
 
-import calendar
 import datetime
 import subprocess
-import sys
+import re
 import time
 from collections import namedtuple
-
-try:
-    from psutil import boot_time
-except ModuleNotFoundError:
-    print('Error: missing "psutil" library.')
-    print('---')
-    subprocess.run('pbcopy', universal_newlines=True, input=f'{sys.executable} -m pip install psutil')
-    print('Fix copied to clipboard. Paste on terminal and run.')
-    exit(1)
 
 def get_timestamp(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %k:%M:%S')
 
-def get_duration_tuple(days=0, hours=0, minutes=0, seconds=0):
-    duration = namedtuple('duration', 'days hours minutes seconds')
-    return duration(days=days, hours=hours, minutes=minutes, seconds=seconds)
+def get_command_output(command):
+    previous = None
+    for command in re.split(r'\s*\|\s*', command):
+        cmd = re.split(r'\s+', command)
+        p = subprocess.Popen(cmd, stdin=(previous.stdout if previous else None), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        previous = p
+    return p.stdout.read().strip().decode(), p.stderr.read().strip().decode()
 
-def get_plural(count, string):
-    return string if count == 1 else f'{string}s'
+def get_duration(seconds):
+    try:
+        seconds = int(seconds)
+        days = int(seconds / 86400)
+        hours = int(((seconds - (days * 86400)) / 3600))
+        minutes = int(((seconds - days * 86400 - hours * 3600) / 60))
+        secs = int((seconds - (days * 86400) - (hours * 3600) - (minutes * 60)))
 
-def get_duration(boot_time):
-    days, hours, minutes, seconds = 0, 0, 0, 0
-    delta = datetime.datetime.fromtimestamp(int(time.time())) - datetime.datetime.fromtimestamp(boot_time)
-    days = delta.days
-    hours, remainder = divmod(delta.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return get_duration_tuple(days=days, hours=hours, minutes=minutes, seconds=seconds)
+        duration = namedtuple('duration', 'days hours minutes seconds')
+        return duration(days=days, hours=hours, minutes=minutes, seconds=secs)
+    except:
+        return None
+
+def get_boot_time():
+    stdout, stderr = get_command_output('sysctl -n kern.boottime')
+    if stderr:
+        return None
+    pattern = re.compile(r'sec = ([0-9]{10,13})')
+    match = re.search(pattern, stdout)
+    if match:
+        timestamp = int(match.group(1))
+        return timestamp
+    return None
+
+def get_duration_tuple():
+    boot_time = get_boot_time()
+    if boot_time:
+        return boot_time, get_duration(int(time.time()) - boot_time)
+    else:
+        return None, None
 
 def main():
-    uptime = []
-    duration = get_duration(boot_time())
-    if duration.days > 0:
-        uptime.append(f'{duration.days} {get_plural(duration.days, "day")}')
-    uptime.append(f'{str(duration.hours).zfill(2)}:{str(duration.minutes).zfill(2)}')
-    print(f'up {" ".join(uptime)}')
-    print('---')
-    print(f'Last boot: {datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")}')
+    boot_time, duration_tuple = get_duration_tuple()
+    if duration_tuple:
+        uptime = []
+        if duration_tuple.days > 0:
+            uptime.append(f'{duration_tuple.days} {"day" if duration_tuple.days == 1 else "days"}')
+        uptime.append(f'{str(duration_tuple.hours).zfill(2)}:{str(duration_tuple.minutes).zfill(2)}')
+        print(f'up {" ".join(uptime)}')
+        print('---')
+        print(f'Last boot: {datetime.datetime.fromtimestamp(boot_time).strftime("%Y-%m-%d %H:%M:%S")}')
+    else:
+        print('Uptime: N/A')
+        print('---')
+        print('Failed to determine boot time')
 
 if __name__ == '__main__':
     main()
