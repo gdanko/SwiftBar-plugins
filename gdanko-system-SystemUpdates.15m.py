4f36f1201e20105a1408a56f7adab5bac5f559ac
gdanko <gdanko@protonmail.com>
2025-01-21 12:16:57 -0800

    Use plugin.py

--- gdanko-system-SystemUpdates.15m.py
+++ gdanko-system-SystemUpdates.15m.py
@@ -8,36 +8,23 @@
 # <xbar.dependencies>python</xbar.dependencies>
 # <xbar.abouturl>https://github.com/gdanko/xbar-plugins/blob/main/System/gdanko-system-SystemUpdates.15m.py</xbar.abouturl>
 
-import datetime
 import os
-import re
-import subprocess
+import plugin
 import time
-
-def get_timestamp(timestamp):
-    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %k:%M:%S')
-
-def get_command_output(command):
-    previous = None
-    for command in re.split(r'\s*\|\s*', command):
-        cmd = re.split(r'\s+', command)
-        p = subprocess.Popen(cmd, stdin=(previous.stdout if previous else None), stdout=subprocess.PIPE)
-        previous = p
-    return p.stdout.read().strip().decode()
 
 def main():
     os.environ['PATH'] = '/bin:/sbin:/usr/bin:/usr/sbin'
     updates = 0
     command = f'softwareupdate --list'
-    output = get_command_output(command)
-    if output:
-        lines = output.split('\n')
+    stdout, stderr = plugin.get_command_output(command)
+    if stdout:
+        lines = stdout.split('\n')
         for line in lines:
             if '* Label' in line:
                 updates += 1
         print(f'Updates: {updates}')
         print('---')
-        print(f'Updated {get_timestamp(int(time.time()))}')
+        print(f'Updated {plugin.get_timestamp(int(time.time()))}')
         print('---')
         print('Refresh system update data | refresh=true')
     else:
