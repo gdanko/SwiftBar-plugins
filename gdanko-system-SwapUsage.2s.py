c529c798719e53d995ab5c5251d5bf4f6b194709
gdanko <gdanko@protonmail.com>
2025-01-26 09:35:32 -0800

    Use plugin class

--- gdanko-system-SwapUsage.2s.py
+++ gdanko-system-SwapUsage.2s.py
@@ -16,50 +16,88 @@
 # <swiftbar.hideSwiftBar>false</swiftbar.hideSwiftBar>
 
 from collections import namedtuple
+from swiftbar import util
+from swiftbar.plugin import Plugin
+import argparse
 import os
-import plugin
 import re
-import sys
-import time
 
-def pad_float(number):
-    return '{:.2f}'.format(float(number))
+def configure():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', help='Toggle viewing the debug section', required=False, default=False, action='store_true')
+    parser.add_argument('--unit', help='Select the unit to use', required=False)
+    args = parser.parse_args()
+    return args
 
 def get_swap_usage_tuple(total=0, used=0, free=0):
     swap_usage = namedtuple('swap_usage', 'total free used')
     return swap_usage(total=total, free=free, used=used)
 
-def get_defaults(config_dir, plugin_name):
-    vars_file = os.path.join(config_dir, plugin_name) + '.vars.json'
-    default_values = {
-        'VAR_SWAP_USAGE_UNIT': 'Gi',
-    }
-    defaults = plugin.read_config(vars_file, default_values)
-    valid_units = ['K', 'Ki', 'M', 'Mi', 'G', 'Gi', 'T', 'Ti', 'P', 'Pi', 'E', 'Ei']
-    return defaults['VAR_SWAP_USAGE_UNIT'] if defaults['VAR_SWAP_USAGE_UNIT'] in valid_units else 'Gi'
-
 def get_swap_usage():
-    command = 'sysctl -n vm.swapusage'
-    returncode, stdout, _ = plugin.execute_command(command)
-    match = re.search(r'^total = (\d+\.\d+)M\s+used = (\d+\.\d+)M\s+free = (\d+\.\d+)M\s+', stdout)
-    if match:
-        total = int(float(match.group(1))) * 1024 * 1024
-        used = int(float(match.group(2))) * 1024 * 1024
-        free = int(float(match.group(3))) * 1024 * 1024
-        return get_swap_usage_tuple(total=total, free=free, used=used)
+    output = util.get_sysctl('vm.swapusage')
+    if output:
+        match = re.search(r'^total = (\d+\.\d+)M\s+used = (\d+\.\d+)M\s+free = (\d+\.\d+)M\s+', output)
+        if match:
+            total = int(float(match.group(1))) * 1024 * 1024
+            used = int(float(match.group(2))) * 1024 * 1024
+            free = int(float(match.group(3))) * 1024 * 1024
+            return get_swap_usage_tuple(total=total, free=free, used=used)
     return None
 
 def main():
     os.environ['PATH'] = '/bin:/sbin:/usr/bin:/usr/sbin'
-    invoker, config_dir = plugin.get_config_dir()
-    plugin_name = os.path.abspath(sys.argv[0])
-    unit = get_defaults(config_dir, os.path.basename(plugin_name))
-    mem = get_swap_usage()
-    used = plugin.byte_converter(mem.used, unit)
-    total = plugin.byte_converter(mem.total, unit)
-    print(f'Swap: {used} / {total}')
-    print('---')
-    print(f'Updated {plugin.get_timestamp(int(time.time()))}')
+    plugin = Plugin()
+    defaults_dict = {
+        'VAR_SWAP_USAGE_DEBUG_ENABLED': {
+            'default_value': False,
+            'valid_values': [True, False],
+        },
+        'VAR_SWAP_USAGE_UNIT': {
+            'default_value': 'Gi',
+            'valid_values': util.valid_storage_units(),
+        },
+    }
+    plugin.read_config(defaults_dict)
+    args = configure()
+    if args.debug:
+        plugin.update_setting('VAR_SWAP_USAGE_DEBUG_ENABLED', True if plugin.configuration['VAR_SWAP_USAGE_DEBUG_ENABLED'] == False else False)
+    elif args.unit:
+        plugin.update_setting('VAR_SWAP_USAGE_UNIT', args.unit)
+
+    plugin.read_config(defaults_dict)
+    debug_enabled = plugin.configuration['VAR_SWAP_USAGE_DEBUG_ENABLED']
+    unit = plugin.configuration['VAR_SWAP_USAGE_UNIT']
+
+    swap = get_swap_usage()
+    if swap:
+        used = util.byte_converter(swap.used, unit)
+        total = util.byte_converter(swap.total, unit)
+        plugin.print_menu_title(f'Swap: {used} / {total}')
+    else:
+        plugin.print_menu_title('Swap: Failed')
+        plugin.print_menu_separator()
+        plugin.print_menu_item('Failed to gather swap information')
+    plugin.print_menu_separator()
+    plugin.print_update_time()
+    plugin.print_menu_item('Settings')
+    plugin.print_menu_item(
+        f'{"--Disable" if debug_enabled else "--Enable"} debug data',
+        cmd=[plugin.plugin_name, '--debug'],
+        terminal=False,
+        refresh=True,
+    )
+    plugin.print_menu_item('--Unit')
+    for valid_storage_unit in util.valid_storage_units():
+        color = 'blue' if valid_storage_unit == unit else 'black'
+        plugin.print_menu_item(
+            f'----{valid_storage_unit}',
+            color=color,
+            cmd=[plugin.plugin_name, '--unit', valid_storage_unit],
+            refresh=True,
+            terminal=False,
+        )
+    if debug_enabled:
+        plugin.display_debug_data()
 
 if __name__ == '__main__':
     main()
