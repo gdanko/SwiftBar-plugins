b3def6e801f0a22365e846b9613fcdb91f91f4a2
gdanko <gdanko@protonmail.com>
2025-01-27 20:55:01 -0800

    Add help to most functions; use the new params module

--- swiftbar/plugin.py
+++ swiftbar/plugin.py
@@ -2,44 +2,13 @@ from collections import OrderedDict
 from pathlib import Path
 from pprint import pprint
 from swiftbar import util
+from swiftbar.params import Params, ParamsXbar, ParamsSwiftBar
 import json
 import os
 import shutil
 import sys
 import time
 import typing
-
-class Params(typing.TypedDict, total=False):
-    # Text Formatting:
-    ansi: bool
-    color: str
-    emojize: bool
-    font: str
-    length: int
-    md: bool
-    sfcolor: str
-    sfsize: int
-    size: int
-    symbolize: bool
-    trim: bool
-
-    # Visuals:
-    alternate: bool
-    checked: bool
-    dropdown: bool
-    image: str
-    sfimage: str
-    templateImage: str
-    tooltip: str
-
-    # Actions:
-    cmd: list
-    refresh: bool
-    href: str
-    shortcut: str
-    bash: str
-    shell: str
-    terminal: bool
 
 class Writer(typing.Protocol):
     def write(self, _: str, /) -> int: ...
@@ -50,8 +19,8 @@ class Plugin:
         self.invoked_by = None
         self.invoked_by_full = None
 
-        self.get_config_dir()
-        self.create_config_dir()
+        self._get_config_dir()
+        self._create_config_dir()
 
         self.font = 'AndaleMono'
         self.size = 13
@@ -64,7 +33,10 @@ class Plugin:
         self.plugin_basename = os.path.basename(self.plugin_name)
         self.vars_file = os.path.join(self.config_dir, self.plugin_basename) + '.vars.json'
 
-    def get_config_dir(self):
+    def _get_config_dir(self) ->None:
+        """
+        Determine the location of the configuration directory based on self.invoked_by, gotten from the parent PID.
+        """
         ppid = os.getppid()
         self.invoker_pid = ppid
         returncode, stdout, stderr = util.execute_command(f'/bin/ps -o command -p {ppid} | tail -n+2')
@@ -80,7 +52,10 @@ class Plugin:
             else:
                 self.config_dir = os.path.join(Path.home(), 'SwiftBar')
 
-    def create_config_dir(self):
+    def _create_config_dir(self):
+        """
+        Create the configuration directory if it doesn't exist.
+        """
         if not os.path.exists(self.config_dir):
             try:
                 os.makedirs(self.config_dir)
@@ -88,6 +63,9 @@ class Plugin:
                 pass
 
     def _write_default_vars_file(self, defaults_dict):
+        """
+        Write a new JSON variables file from the contents of the defaults_dict sent by the plugin.
+        """
         config_data = {}
         for key, value in defaults_dict.items():
             config_data[key] = value['default_value']
@@ -96,6 +74,9 @@ class Plugin:
             fh.write(json.dumps(config_data, indent=4))
     
     def _rewrite_vars_file(self):
+        """
+        Rewrite the JSON variables file from the contents of self.coniguration.
+        """
         with open(self.vars_file, 'w') as fh:
             fh.write(json.dumps(self.configuration, indent=4))
 
@@ -138,10 +119,17 @@ class Plugin:
             self._rewrite_vars_file()
  
     def write_config(self, contents):
+        # Let's make sure this is not duplicated
+        """
+        Write the JSON variables file.
+        """
         with open(self.vars_file, 'w') as fh:
             fh.write(json.dumps(contents, indent=4))
 
     def update_setting(self, key, value):
+        """
+        Update a given setting for a plugin and rewrite the JSON variables file.
+        """
         if os.path.exists(self.vars_file):
             with open(self.vars_file, 'r') as fh:
                 contents = json.load(fh)
@@ -150,31 +138,39 @@ class Plugin:
                     self.write_config(contents)
 
     def find_longest(self, input) ->int:
+        """
+        Find the longest item in a list or the longest key in a dict. Used for formatting lists.
+        """
         if type(input) == list:
             return max(len(key) for key in input)
         elif type(input) == dict or type(input) == OrderedDict:
             return max(len(key) for key in input.keys())
 
-    def sanitize_for_xbar(self, params: dict={}) ->dict:
-        to_remove = []
-        invalid = [
-            'emojize',
-            'sfimage',
-            'symbolize',
-        ]
-        for param in params:
-            if param in invalid:
-                to_remove.append(param)
-        if len(to_remove) > 0:
-            for param in to_remove:
-                params.pop(param)
-        return params
+    def sanitize_params(self, **params: Params):
+        """
+        Create a new params object based on the value of self.invoked_by. Both xbar and SwiftBar have some unique
+        parameters and this will allow the work to be handled behind the scenes.
+        """
+        sanitized = ParamsXbar() if self.invoked_by == 'xbar' else ParamsSwiftBar()
+        for k, v in params.items():
+            try:
+                sanitized[k] = v
+            except KeyError as e:
+                pass
+        return sanitized
 
     def print_menu_title(self, text: str, *, out: Writer=sys.stdout, **params: Params) ->None:
+        """
+        Print the plugin title in the menu bar.
+        """
+        params = self.sanitize_params(**params)
         params_str = ' '.join(f'{k}={v}' for k, v in params.items())
         print(f'{text} | {params_str}', file=out)
 
     def print_ordered_dict(self, data: OrderedDict, justify: str='right', delimiter: str = '', indent: int=0, *, out: Writer=sys.stdout, **params: Params) ->None:
+        """
+        Render an instance of collections.OrderedDict().
+        """
         indent_str = indent * '-'
         longest = self.find_longest(data)
         params['trim'] = False
@@ -186,8 +182,11 @@ class Plugin:
                 self.print_menu_item(f'{indent_str}{k.rjust(longest)} {delimiter} {v} | {params_str}', **params)
 
     def print_menu_item(self, text: str, *, out: Writer=sys.stdout, **params: Params) ->None:
+        """
+        Generic wrapper to print all non-title menu items.
+        """
         # https://github.com/tmzane/swiftbar-plugins
-        # If python >= 3.11, we can replace **params: Params with **params: typing.Unpack[Params]
+        params = self.sanitize_params(**params)
 
         # Set default font if one isn't configured
         if not 'font' in params:
@@ -201,22 +200,28 @@ class Plugin:
             params['bash'] = f'"{params["cmd"][0]}"'
             for i, arg in enumerate(params['cmd'][1:]):
                 params[f'param{i + 1}'] = f'"{arg}"'
-        if 'cmd' in params:
-            params.pop('cmd')
+            if 'cmd' in params:
+                params.pop('cmd')
 
-        # Special handling for xbar
-        if self.invoked_by == 'xbar':
-            params = self.sanitize_for_xbar(params)
         params_str = ' '.join(f'{k}={v}' for k, v in params.items())
         print(f'{text} | {params_str}', file=out)
 
     def print_menu_separator(self, *, out: Writer = sys.stdout) -> None:
+        """
+        Print a menu separator.
+        """
         print('---', file=out)
     
     def print_update_time(self, *, out: Writer = sys.stdout) -> None:
+        """
+        Print the updated time in human format.
+        """
         self.print_menu_item(f'Updated {util.get_timestamp(int(time.time()))}')
 
     def display_debug_data(self):
+        """
+        Create a menu item to display plugin debug information.
+        """
         pv = sys.version_info
         os_version = util.get_macos_version()
         total_mem = util.get_sysctl('hw.memsize')
