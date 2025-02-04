67400c89a24f3335c86d7ef4b74403aba1448b1f
gdanko <gdanko@protonmail.com>
2025-02-04 09:20:13 -0800

    Add LF

--- gdanko-system-BrewOutdated.30m.py
+++ gdanko-system-BrewOutdated.30m.py
@@ -77,6 +77,7 @@ def main() -> None:
             'title': 'the "Debugging" menu',                
         },
     }
+
     plugin.read_config()
     plugin.generate_args()
     plugin.update_json_from_args()
