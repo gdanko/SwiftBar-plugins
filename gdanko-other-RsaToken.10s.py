4ec1656132aacb4d4a3ca18c46a13f4e09b4c140
Gary Danko <gary.danko@servicenow.com>
2025-02-08 16:32:54 -0800

    Now this is much better

--- gdanko-other-RsaToken.10s.py
+++ gdanko-other-RsaToken.10s.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # <xbar.title>RsaToken</xbar.title>
-# <xbar.version>v0.3.0</xbar.version>
+# <xbar.version>v0.3.1</xbar.version>
 # <xbar.author>Gary Danko</xbar.author>
 # <xbar.author.github>gdanko</xbar.author.github>
 # <xbar.desc>fetches current rsa token, and allows you to copy your pin from keychain to your paste buffer</xbar.desc>
@@ -35,7 +35,7 @@
 
 from swiftbar import images, util
 from swiftbar.plugin import Plugin
-from typing import Dict, List, Union
+from typing import Any, Dict, List, Union
 import shutil
 
 def process_actions(plugin: Plugin=None):
@@ -51,15 +51,6 @@ def process_actions(plugin: Plugin=None):
             )
             plugin.print_menu_separator()
 
-def setup() -> str:
-    brew = shutil.which('brew')
-    if not brew:
-        return 'homebrew not installed'
-
-    stoken = shutil.which('stoken')
-    if not stoken:
-        return 'stoken not installed - brew install stoken'
-
 def refresh_token() -> Union[str, None]:
     _, stdout, stderr = util.execute_command('security find-generic-password -w -s rsatoken | stoken --stdin')
     if stderr:
@@ -72,9 +63,18 @@ def get_item(key: str=None) -> Union[str, None]:
         return None, stderr
     return stdout, None
 
-def get_data() -> Union[Dict[str, str], List[str]]:
+def get_data():
     errors = []
     output = {}
+
+    brew = shutil.which('brew')
+    if not brew:
+        return ['homebrew not installed'], None
+
+    stoken = shutil.which('stoken')
+    if not stoken:
+        return ['stoken not installed - brew install stoken'], None
+
     output['token'], error = refresh_token()
     if error:
         errors.append(error)
@@ -132,28 +132,22 @@ def main() -> None:
     }
     plugin.setup()
 
-    plugin.print_menu_title('RSA Token')
-    process_actions(plugin=plugin)
-
-    error = setup()
-    if error:
+    output, errors = get_data()
+    if len(errors) > 0:
         plugin.print_menu_title('RSA Token Error')
-        plugin.print_menu_item(error)
+        for error in errors:
+            plugin.print_menu_item(error)
     else:
-        output, errors = get_data()
-        if len(errors) > 0:
-            plugin.print_menu_title('RSA Token Error')
-            for error in errors:
-                plugin.print_menu_item(error)
-        else:
-            if plugin.args.token:
-                pbcopy(f'{output["rsatoken-pin"]}{output["token"]}')
-            elif plugin.args.snad:
-                pbcopy(output['snad'])
-            elif plugin.args.ldap:
-                pbcopy(output['snc.bssh.ldap_pass'])
-            elif plugin.args.next:
-                refresh_token()
+        plugin.print_menu_title('RSA Token')
+        process_actions(plugin=plugin)
+        if plugin.args.token:
+            pbcopy(f'{output["rsatoken-pin"]}{output["token"]}')
+        elif plugin.args.snad:
+            pbcopy(output['snad'])
+        elif plugin.args.ldap:
+            pbcopy(output['snc.bssh.ldap_pass'])
+        elif plugin.args.next:
+            refresh_token()   
     plugin.render_footer()
 
 if __name__ == '__main__':
